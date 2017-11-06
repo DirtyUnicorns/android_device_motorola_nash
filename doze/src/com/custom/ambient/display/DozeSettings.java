b9a55f3fef599aff4801fe154a1766072d9c969b
Alex Cruz <du.alexcruz@gmail.com>
2017-11-06 16:45:01 -0500

    Remove Doze dialog
    We don't need this, nobody does. Most users are aware of ambient display and
    what is capable of.
    
    Change-Id: I30abd53cfae1101157b92bf9b869e6469915c2f2

--- doze/src/com/custom/ambient/display/DozeSettings.java
+++ doze/src/com/custom/ambient/display/DozeSettings.java
@@ -18,12 +18,7 @@ package com.custom.ambient.display;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -37,14 +32,11 @@ import android.view.MenuItem;
 public class DozeSettings extends PreferenceActivity implements OnPreferenceChangeListener {
 
     private Context mContext;
-    private SharedPreferences mPreferences;
 
     private SwitchPreference mAmbientDisplayPreference;
     private SwitchPreference mPickUpPreference;
-    private SwitchPreference mTiltAlwaysPreference;
     private SwitchPreference mHandwavePreference;
     private SwitchPreference mPocketPreference;
-    private SwitchPreference mProximityAlwaysPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,21 +45,11 @@ public class DozeSettings extends PreferenceActivity implements OnPreferenceChan
         mContext = getApplicationContext();
         boolean dozeEnabled = Utils.isDozeEnabled(mContext);
 
-        // get shared preference
-        mPreferences = mContext.getSharedPreferences("doze_settings", Activity.MODE_PRIVATE);
-        if (savedInstanceState == null && !mPreferences.getBoolean("first_help_shown", false)) {
-            showHelp();
-        }
-
         mAmbientDisplayPreference =
             (SwitchPreference) findPreference(Utils.AMBIENT_DISPLAY_KEY);
         // Read from DOZE_ENABLED secure setting
         mAmbientDisplayPreference.setChecked(dozeEnabled);
         mAmbientDisplayPreference.setOnPreferenceChangeListener(this);
-
-        mTiltAlwaysPreference =
-            (SwitchPreference) findPreference(Utils.TILT_ALWAYS_KEY);
-        mTiltAlwaysPreference.setOnPreferenceChangeListener(this);
 
         mPickUpPreference =
             (SwitchPreference) findPreference(Utils.PICK_UP_KEY);
@@ -81,20 +63,13 @@ public class DozeSettings extends PreferenceActivity implements OnPreferenceChan
             (SwitchPreference) findPreference(Utils.GESTURE_POCKET_KEY);
         mPocketPreference.setOnPreferenceChangeListener(this);
 
-        mProximityAlwaysPreference =
-            (SwitchPreference) findPreference(Utils.PROXIMITY_ALWAYS_KEY);
-        mProximityAlwaysPreference.setOnPreferenceChangeListener(this);
-
         final ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-
-        updateAlwaysEnabledPreference();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateAlwaysEnabledPreference();
     }
 
     @Override
@@ -116,63 +91,17 @@ public class DozeSettings extends PreferenceActivity implements OnPreferenceChan
             return true;
         } else if (Utils.PICK_UP_KEY.equals(key)) {
             mPickUpPreference.setChecked(value);
-            updateAlwaysEnabledPreference();
             Utils.startService(mContext);
-            return true;
-        } else if (Utils.TILT_ALWAYS_KEY.equals(key)) {
-            mTiltAlwaysPreference.setChecked(value);
             return true;
         } else if (Utils.GESTURE_HAND_WAVE_KEY.equals(key)) {
             mHandwavePreference.setChecked(value);
-            updateAlwaysEnabledPreference();
             Utils.startService(mContext);
             return true;
         } else if (Utils.GESTURE_POCKET_KEY.equals(key)) {
             mPocketPreference.setChecked(value);
-            updateAlwaysEnabledPreference();
             Utils.startService(mContext);
-            return true;
-        } else if (Utils.PROXIMITY_ALWAYS_KEY.equals(key)) {
-            mProximityAlwaysPreference.setChecked(value);
             return true;
         }
         return false;
-    }
-
-    public static class HelpDialogFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.doze_settings_help_title)
-                    .setMessage(R.string.doze_settings_help_text)
-                    .setNegativeButton(R.string.dlg_ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    })
-                    .create();
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-            getActivity().getSharedPreferences("doze_settings", Activity.MODE_PRIVATE)
-                    .edit()
-                    .putBoolean("first_help_shown", true)
-                    .commit();
-        }
-    }
-
-    private void showHelp() {
-        HelpDialogFragment fragment = new HelpDialogFragment();
-        fragment.show(getFragmentManager(), "help_dialog");
-    }
-
-    private void updateAlwaysEnabledPreference() {
-        boolean tiltEnabled = Utils.pickUpEnabled(mContext);
-        boolean proximityEnabled = Utils.handwaveGestureEnabled(mContext)
-                || Utils.pocketGestureEnabled(mContext);
-        mTiltAlwaysPreference.setEnabled(tiltEnabled);
-        mProximityAlwaysPreference.setEnabled(proximityEnabled);
     }
 }
