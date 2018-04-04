e27f112a94ddeb64afd9a48e9e71622c1d7d746a
Josh Correll <jbatscmb@gmail.com>
2018-04-04 15:24:28 -0600

    MotoActions
    initial port from potter
    
    Change-Id: I56bb283397a3ab5b066c41473dd6461ce0ee3453

--- device.mk
+++ device.mk
@@ -254,6 +254,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
