f3828c7be063b8740223890b11ee2e3dfaf91c01
Alex Cruz <du.alexcruz@gmail.com>
2018-04-04 17:47:34 -0400

    Remove fingerprint and screen off gestures
    Change-Id: I7eb1aba30077825f431e6ae29d7f943454d68a4d

--- MotoActions/src/com/moto/actions/MotoActionsSearchIndexablesProvider.java
+++ MotoActions/src/com/moto/actions/MotoActionsSearchIndexablesProvider.java
@@ -49,23 +49,10 @@ public class MotoActionsSearchIndexablesProvider extends SearchIndexablesProvide
                     GestureSettingsActivity.class.getName(),
                     R.drawable.ic_settings_gestures)));
 
-        cursor.addRow(generateResourceRef(new SearchIndexableResource(1, R.xml.fp_gesture_panel,
-                    FPGestureSettingsActivity.class.getName(),
-                    R.drawable.ic_settings_fingerprint)));
-        cursor.addRow(generateResourceRef(new SearchIndexableResource(1, R.xml.fp_gesture_panel_indexable,
-                    FPGestureSettingsActivity.class.getName(),
-                    R.drawable.ic_settings_fingerprint)));
-
-        cursor.addRow(generateResourceRef(new SearchIndexableResource(1, R.xml.screen_off_gesture_panel,
-                    ScreenOffGestureSettingsActivity.class.getName(),
-                    R.drawable.ic_settings_screen_off_gestures)));
-        cursor.addRow(generateResourceRef(new SearchIndexableResource(1, R.xml.screen_off_gesture_panel_indexable,
-                    ScreenOffGestureSettingsActivity.class.getName(),
-                    R.drawable.ic_settings_screen_off_gestures)));
-
         cursor.addRow(generateResourceRef(new SearchIndexableResource(1, R.xml.doze_panel,
                     DozeSettingsActivity.class.getName(),
                     R.drawable.ic_settings_doze)));
+
         cursor.addRow(generateResourceRef(new SearchIndexableResource(1, R.xml.doze_panel_indexable,
                     DozeSettingsActivity.class.getName(),
                     R.drawable.ic_settings_doze)));
