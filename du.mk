2af45fb3489b025fa3461470fae74532bfc3f448
Josh Correll <jbatscmb@gmail.com>
2018-04-05 01:21:25 -0600

    Add dependency
    cleanup du.mk
    
    Change-Id: I690c2e3de6175b88fae3dea53d3707f0f1f6ae78

--- du.mk
+++ du.mk
@@ -1,9 +1,8 @@
-# Inherit some common Lineage stuff.
+# Inherit some common DU stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/motorola/nash/nash-vendor.mk)
