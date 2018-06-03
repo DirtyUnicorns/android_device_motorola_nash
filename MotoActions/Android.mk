5004c0d3e7f165c4c0e48087e3d214ab6e3eee7b
Vachounet <vachounet@live.fr>
2018-06-03 16:59:00 +0200

    nash: Move MotoActions to vendor

--- MotoActions/Android.mk
+++ MotoActions/Android.mk
@@ -8,7 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := MotoActions
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v14-preference \
