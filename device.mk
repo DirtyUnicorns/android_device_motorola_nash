a5e693e9a5554de5b14b089bdee9611876780a4f
Erfan Abdi <erfangplus@gmail.com>
2018-08-30 17:22:12 +0300

    nash: Update blobs from SDM660
    * sed-ing some cam blobs to use in vendor libgui.so
      (libPui.so and libgui_vendor.so, both built with vndk-28).
    
    Change-Id: I3fd016a72c63919a41f64e80ff3c2732a64b78ca
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -127,7 +127,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -152,6 +153,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1-service-qti \
     libgnss \
     libgnsspps \
+    libloc_api_v02 \
+    libloc_ds_api \
     libvehiclenetwork-native
 
 PRODUCT_PACKAGES += \
@@ -421,9 +424,6 @@ PRODUCT_PACKAGES += \
     libicuuc.vendor \
     libstdc++.vendor \
     vndk_package
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gnss@1.0-v27.so
 
 # Weaver
 PRODUCT_PACKAGES += \
